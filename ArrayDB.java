f9692a73d3a567d2ea8f9eb106ee49ca1255c89e
toninodev <148453865+toninodev@users.noreply.github.com>
2023-12-11 12:17:57 +0100

    Add files via upload
    Clase ArrayDB - Xestor de Base de Datos con Array
    A clase ArrayDB é un xestor simple de base de datos que utiliza un array de rexistros (Record) para almacenar información.
    
    SE PIDE codificar as ArrayDB creando as seuintes clases no package database.
    
    Clase Record - Rexistro
    A clase Record representa un rexistro básico na base de datos. Cada rexistro contén un estado de validez (boolean isvalid), datos (Object data), e proporciona métodos para comprobar se foi eliminado, eliminar o rexistro, obter os datos e establecer novos datos.
    
    Métodos:
    boolea isDeleted(): Devolve true se o rexistro foi eliminado, false en caso contrario. Un rexistro está borrado si o seu atributo isvalid é false
    void delete(): Establece o estado de validez como false, eliminando así o rexistro.
    Object getData(): Devolve os datos almacenados no rexistro.
    void setData(Object obj): Establece novos datos no rexistro e marca o rexistro como válido.
    Clase ArrayDB - Xestor de Base de Datos
    A clase ArrayDB é un xestor de base de datos que utiliza un array de rexistros (obxectos Record) para almacenar información. Os métodos principais permiten engadir, eliminar, listar, buscar e modificar obxectos na base de datos.
    
    Atributos:
    Record[] data: Conxunto de obxectos Record que gardarán os datos a xestionar
    int iterate: NON é un atributo realmente. É unha variable auxiliar para poder percorrer os datos válidos almacenados.
    Construtor Público:
    ArrayDB(int capacity): Inicializa o atributo data creando un array de capacity elementos, e gardando nel os obxectos Record necesarios para gardar os datos a xestionar.
    Métodos Públicos (API):
    Object add(Object obj): Engade un obxecto á base de datos. Lanza unha excepción se non hai espazo na base de datos ou se o obxecto xa existe. Retorna o obxecto engadido.
    Object delete(Object obj): Elimina un obxecto da base de datos. Lanza unha excepción se o obxecto non existe. Retorna o obxecto eliminado
    Object[] list(): Retorna un array con todos os obxectos na base de datos (non implementado. Lanza UnsupportedOperationException).
    Object[] findAll(Filter f): Retorna un array cos obxectos que cumplen o criterio do filtro (non implementado. Lanza UnsupportedOperationException).
    Object findOne(Filter f): Retorna o primeiro obxecto que cumpre o criterio do filtro.
    void startIteration(): Comeza unha nova iteración sobre a base de datos. Despois de chamar a este método, a chamada a next() retornará o primeiro rexistro
    Object next(): Retorna o obxecto seguinte ao retornado con anterioridade. Ignora os rexistros borrados.
    Object modify(Object obj): Modifica un obxecto existente na base de datos. Lanza unha excepción se o obxecto non existe. Retorna o obxecto tal e como era antes de "ser modificado".
    Métodos Privados:
    int getInsertPosition(Object obj): Obtén a posición para engadir un obxecto. Lanza unha excepción se o obxecto xa existe. Retorna -1 si non queda espazo no array
    int getPosition(Object obj): Obtén a posición dun obxecto na base de datos. Retorna -1 si non existe.

---
+++ ArrayDB.java
@@ -0,0 +1,137 @@
+package database;
+
+public class ArrayDB extends Database {
+    private Record[] data;
+    private int iterate;
+    
+    public ArrayDB(int capacity) {
+        data=new Record[capacity];
+        for(int idx=0;idx<capacity;idx++) data[idx]=new Record();
+    }
+    
+    @Override
+    public Object add(Object obj) throws DBException {
+        int position=getInsertPosition(obj);
+        if (position<0) throw new DBException(DbError.ERRINSERT,"Non queda espazo na base de datos para engadir "+obj);
+        data[position].setData(obj);
+        return obj;
+    }
+    
+    @Override
+    public Object delete(Object obj) throws DBException {
+        int position=getPosition(obj);
+        if (position<0) throw new DBException(DbError.NOTEXISTS,"Non se pudo eliminar "+obj);
+        data[position].delete();
+        return data[position].getData();
+    }
+    
+    @Override
+    public Object[] list() {
+        // Necesitamos usar estruturas dinámicas de almacenamento que veremos no futuro
+        throw new UnsupportedOperationException("Operación non soportada");
+    }
+    
+    @Override
+    public Object[] findAll(Filter f) throws DBException {
+        // Necesitamos usar estruturas dinámicas de almacenamento que veremos no futuro
+        throw new UnsupportedOperationException("Operación non soportada");
+
+    }
+    
+    @Override
+    public Object find(Object search) throws DBException {
+        // Usamos unha clase anónima porque é moi cómodo
+        //
+        return findOne(new Filter() {
+            @Override
+            public boolean isValid(Object obj) {
+                return search.equals(obj);
+            }
+        });
+    }
+    
+    @Override
+    public Object findOne(Filter f) throws DBException {
+        for (Record r : data) {
+            Object info = r.getData();
+            if (!r.isDeleted() && f.isValid(info)) {
+                return info;
+            }
+        }
+        return null;
+    }
+    
+    public void startIteration() {
+        iterate=0;
+    }
+    
+    public Object next() {
+        Object result=null;
+        while (iterate < data.length && result==null) {
+            if (!data[iterate].isDeleted()) result=data[iterate].getData();
+            iterate++;
+        }
+        return result;
+    }
+    
+    @Override
+    public Object modify(Object obj) throws DBException {
+        int idx=getPosition(obj);
+        if (idx<0) throw new DBException(DbError.NOTEXISTS,"modificando "+obj);
+        Record r=data[idx];
+        r.setData(obj);
+        return r;
+    }
+    
+    private int getInsertPosition(Object obj) throws DBException {
+        int ipos=-1;
+        for(int idx=0;idx<data.length;idx++) {
+            if (!data[idx].isDeleted() && data[idx].getData().equals(obj)) throw new DBException(DbError.DUPLICATE,"buscando posición de inserción");
+            if (ipos<0 && data[idx].isDeleted()) ipos=idx;
+        }
+        return ipos;
+    }
+    
+    private int getPosition(Object obj) throws DBException {
+        int ipos=-1;
+        for(int idx=0;idx<data.length && ipos<0;idx++) {
+            if (!data[idx].isDeleted() && data[idx].getData().equals(obj)) ipos=idx;
+        }
+        return ipos;
+    }
+    
+    public static void main(String[] args) throws DBException {
+        ArrayDB adb=new ArrayDB(100);
+
+        Object obj;
+        try {
+            
+            adb.add("Prueba");
+            adb.add(1500);
+            adb.add("Exemplo....");
+            adb.add(198.567);
+            
+            // Exemplo de visualización de todos os obxectos na base de datos
+
+            adb.startIteration();
+            while((obj=adb.next())!=null) System.out.println(obj);
+            
+            adb.delete(1500);
+            
+            System.out.println("FIND: "+adb.find("Exemplo...."));
+     
+            // Outro modo
+            adb.startIteration();
+            obj=adb.next();
+            while(obj!=null) {
+                System.out.println(obj);
+                obj=adb.next();
+            }
+            
+            adb.add("Prueba");
+        } catch (DBException ex) {
+            System.out.println("Error: "+String.format(ex.getMessage(),"'elemento'")+" --> "+ex.getReason());
+            adb.delete("Esto lanza unha Exception que non se captura");
+        }
+    }
+}
